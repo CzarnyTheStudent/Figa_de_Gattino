735203a9e98dbce6c9937a48aed1b52830b9a7ed
Jaksa Utsendari <jcudzilo@edu.cdv.pl>
2024-04-06 22:16:30 +0200

    Pokazywanie Golda

--- Assets/Code/Cat/CatGoldMenagement.cs
+++ Assets/Code/Cat/CatGoldMenagement.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CatGoldMenagement : MonoBehaviour
 {
-   
     public int catGoldAmount = 0;
+    public TextMeshProUGUI goldText; // Referencja do obiektu TextMeshPro
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,12 +14,18 @@ public class CatGoldMenagement : MonoBehaviour
         {
             AddGold(20);
             Destroy(other.gameObject);
+            UpdateGoldText(); // Aktualizuj tekst na obiekcie TextMeshPro
         }
     }
-
 
     private void AddGold(int amount)
     {
         catGoldAmount += amount;
+    }
+
+    private void UpdateGoldText()
+    {
+        // Aktualizuj tekst na obiekcie TextMeshPro
+        goldText.text = "Cat's Gold: " + catGoldAmount.ToString();
     }
 }
