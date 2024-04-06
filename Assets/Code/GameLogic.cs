e3ba40973c50891058c5a3509d8cf5ef5d42da92
CzarnyTheStudent <kpabiniak@edu.cdv.pl>
2024-04-06 16:48:40 +0200

    WeaponBase

--- Assets/Code/GameLogic.cs
+++ Assets/Code/GameLogic.cs
@@ -7,11 +7,15 @@ public class GameLogic : MonoBehaviour
 {
     [SerializeField]private GameObject figa;
     [SerializeField]private GameObject leonardo;
+    [SerializeField]private Spawner spawner;
 
 
     private void Start()
     {
         Vector3 respawnLeo = figa.transform.position;
         GameObject leonardoInstance = Instantiate(leonardo, respawnLeo + new Vector3(5, 0 , 0), Quaternion.identity);
+
+        spawner = figa.GetComponent<Spawner>();
+        spawner.leonardoRef = leonardoInstance;
     }
 }
