0f7cc7925db142874b835067af81b77e27f5e3b4
Jaksa Utsendari <jcudzilo@edu.cdv.pl>
2024-04-07 14:09:38 +0200

    XXX

---
+++ Assets/Code/GiftsScript.cs
@@ -0,0 +1,38 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftsScript : MonoBehaviour
+{
+    public CatGoldMenagement catWealthModification;
+    public CatHealth catHealthModification;
+    public CatMove catMoveModification;
+    public Health leonardoHealthModification;
+
+     void ChooseLeft1()
+    {
+        public Image[] hearts;
+        catHealthModification.catCurrentHealth = 9;
+
+        for (int i = 0; i < catHealthModification.catMaxHealth, i++)
+        {
+            hearts[i].gameObject.SetActive(true);
+        }
+    }
+
+     void ChooseRight1()
+    {
+        catMoveModification.moveSpeed++;
+    }
+
+    void ChooseLeft2()
+    {
+        catWealthModification.AddGold(20);
+    }
+
+    void ChooseRight2()
+    {
+        leonardoHealthModification.health = 110;
+    }
+}
+*/
